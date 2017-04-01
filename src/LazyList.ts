aaf8c7cc20a32b559248337b93313f05dc9f925d
Antonio Aversa <differenziale@gmail.com>
2017-04-01 23:24:01 +0200

    LazyList: nil implementation + nil UTs + except UTs

--- src/LazyList.ts
+++ src/LazyList.ts
@@ -1,13 +1,19 @@
 export type Generator<A> = () => IterableIterator<A>
 type Predicate<A> = (item: A) => boolean
 
+export function nil<A>() : Generator<A> {
+  return function* () : IterableIterator<A> {
+    yield* [][Symbol.iterator]();
+  };
+}
+
 export function cons<A>(value: A, list: Generator<A> | A) {
   return function* () {
     yield value;
     yield* typeof list === "function" ?
       list() :
       [list][Symbol.iterator]();
-  }
+  };
 }
 
 export function except<A>(predicate : Predicate<A>, list : Generator<A>) {
@@ -17,7 +23,7 @@ export function except<A>(predicate : Predicate<A>, list : Generator<A>) {
         yield item;
       }
     }
-  }
+  };
 }
 
 export function withValue<A>(predicate: Predicate<A>, newValue : A, list: Generator<A>) {
@@ -28,7 +34,7 @@ export function withValue<A>(predicate: Predicate<A>, newValue : A, list: Genera
         yield item;
       }
     }
-  }
+  };
 }
 
 export function find<A>(predicate: Predicate<A>, list: Generator<A>) : A {
--- test/LazyList.ts
+++ test/LazyList.ts
@@ -1,7 +1,22 @@
 import should = require('should');
-import { cons } from "../src/LazyList";
+import { Generator, nil, cons, except } from "../src/LazyList";
 
 describe("LazyList", () => {
+    describe("#nil", () => {
+        it("should give back an empty LazyList", () => {
+            const lazyList = nil();
+            should(lazyList).be.not.null();
+            lazyList.should.be.a.Function();
+        });
+
+        it("should generate a zero-elements LazyList generator, when invoked and the resulting LazyList is invoked", () => {
+            const lazyListIterator = nil()();
+            should(lazyListIterator).be.a.iterator();
+            const lazyListValues = [...lazyListIterator];
+            lazyListValues.should.be.empty();
+        });
+    });
+
     describe("#cons", () => {
         it("should give back a LazyList of two elements (which is a 'generator builder'), when invoked with two primitive values", () => {
             const lazyList = cons(1, 2);
@@ -32,5 +47,58 @@ describe("LazyList", () => {
             lazyListValues.length.should.equal(3);
             lazyListValues.should.deepEqual([1,2,3]);
         });
+
+        it("should generate an N-elements list, when deep nested", () => {
+            let lazyList = nil();
+            for(let i = 9; i >= 0; i--)
+                lazyList = cons(i, lazyList);
+
+            const lazyListValues = [...lazyList()];
+            lazyListValues.should.not.be.empty();
+            lazyListValues.length.should.equal(10);
+            lazyListValues.should.deepEqual([...Array(10).keys()]);
+        });
     }); 
+
+    describe("#except", () => {
+        let lazyList : Generator<number>;
+        let lazyListValues : number[];
+        
+        beforeEach (done => {
+            lazyList = nil<number>();
+            for(let i = 9; i >= 0; i--)
+                lazyList = cons(i, lazyList);
+            lazyListValues = [...lazyList()];    
+            done();
+        });
+
+        it("should filter out the specified element from a lazy list, when present", () => {            
+            const lazyListWithout2 = except(x => x == 2, lazyList);
+            const lazyListWithout2Values = [...lazyListWithout2()];
+            lazyListWithout2Values.should.not.be.empty();
+            lazyListWithout2Values.length.should.equal(9);
+            lazyListWithout2Values.should.not.containEql(2);
+        });
+
+        it("should filter out a group of element from a lazy list, based on a predicate", () => {
+            const lazyListWithoutEvenElements = except(x => x % 2 == 0, lazyList);
+            const lazyListWithoutEvenElementsValues = [...lazyListWithoutEvenElements()];
+            lazyListWithoutEvenElementsValues.should.not.be.empty();
+            lazyListWithoutEvenElementsValues.length.should.equal(5);
+            lazyListWithoutEvenElementsValues.should.not.containEql(2);
+            lazyListWithoutEvenElementsValues.should.not.containEql(4);
+        });
+
+        it("should filter out nothing, when the predicate is never satified", () => {
+            const lazyListWithAllTheElements = except(x => false, lazyList);
+            const lazyListWithAllTheElementsValues = [...lazyListWithAllTheElements()];
+            lazyListWithAllTheElementsValues.should.deepEqual(lazyListValues);
+        });
+
+        it("should filter out everything, when the predicate is always satified", () => {
+            const lazyListWithAllTheElements = except(x => true, lazyList);
+            const lazyListWithAllTheElementsValues = [...lazyListWithAllTheElements()];
+            lazyListWithAllTheElementsValues.should.be.empty();
+        });
+    });
 });
