876d19015057e13e7b38e4636fe36b9c9bdcfd25
Andrea Guarino <andrea.guarino.1989@gmail.com>
2017-04-03 00:12:01 +0200

    Change set implementation for Zipper

--- src/Zipper.ts
+++ src/Zipper.ts
@@ -20,11 +20,11 @@ export function init<A>(initObj: Object): Zipper<A> {
   return [buildTree(initObj), <LazyList.Generator<Crumb<A>>>LazyList.nil()];
 }
 
-function buildTree(initObj: Object) : CompositeValue<any>{
+function buildTree(initObj: Object): CompositeValue<any> {
   return CompositeValue(
     Object.keys(initObj)
-    .reduce<LazyList.Generator<any>>(
-      (list, k : string) => {
+      .reduce<LazyList.Generator<any>>(
+      (list, k: string) => {
         const v = initObj[k];
         if (typeof v === "object" && !Array.isArray(v)) {
           return LazyList.cons(NamedValue(k, buildTree(v)), list);
@@ -33,22 +33,24 @@ function buildTree(initObj: Object) : CompositeValue<any>{
         }
       },
       LazyList.nil())
-    );
+  );
 }
 
-function navigate<A>(key: string, zipper: Zipper<A>): Zipper<A> {
+function focus<A>(path: string[], zipper: Zipper<A>): Zipper<A> {
   const currValue = zipper[0];
-  switch (currValue.kind) {
-    case "value":
-      return zipper;
-    case "object": {
-      const nextValue = LazyList.find(x => x.key === key, currValue.values);
-      const nextCrumb = Crumb(key, LazyList.except(x => x.key === key, currValue.values));
-      return nextValue ?
-        [nextValue.value, LazyList.cons(nextCrumb, zipper[1])]
-        : zipper;
+  return path.reduce<Zipper<A>>(function (resultZipper, key) {
+    switch (currValue.kind) {
+      case "value":
+        return resultZipper;
+      case "object": {
+        const nextValue = LazyList.find(x => x.key === key, currValue.values);
+        const nextCrumb = Crumb(key, LazyList.except(x => x.key === key, currValue.values));
+        return nextValue ?
+          [nextValue.value, LazyList.cons(nextCrumb, resultZipper[1])]
+          : resultZipper;
+      }
     }
-  }
+  }, zipper);
 }
 
 /**
@@ -72,7 +74,7 @@ export function get<A>(key: string, zipper: Zipper<A>): A | Zipper<A> {
         case "value":
           return nextValue.value;
         case "object":
-          return [nextValue, null];
+          return [nextValue, LazyList.nil];
       }
   }
 }
@@ -88,19 +90,20 @@ export function get<A>(key: string, zipper: Zipper<A>): A | Zipper<A> {
  * ```
  */
 
-function _set<A>(key: string, newValue: A, zipper: Zipper<A>): Promise<Zipper<A>> {
-  const currValue = zipper[0];
-  switch (currValue.kind) {
-    case "value":
-      return Promise.resolve(zipper);
-    case "object":
-      const modifiedValues = LazyList.withValue(
-        x => x.key === key,
-        NamedValue(key, SimpleValue(newValue)),
-        currValue.values)
-      return Promise.resolve(
-        <Zipper<A>>[CompositeValue(modifiedValues), zipper[1]]
-      )
+function _set<A>(key: string | string[], newValue: A, zipper: Zipper<A>): Promise<Zipper<A>> {
+  const updatedZipper = Array.isArray(key)
+    ? focus(key, zipper)
+    : zipper;
+  const lastKey = Array.isArray(key)
+    ? key[key.length - 1]
+    : key;
+  const currValue = updatedZipper[0];
+  if (typeof newValue === "object") {
+    return Promise.resolve(
+      <Zipper<A>>[buildTree(newValue), updatedZipper[1]]);
+  } else {
+    return Promise.resolve(
+      <Zipper<A>>[SimpleValue(newValue), updatedZipper[1]]);
   }
 };
 
